509d88e900e7946e155f8a8da4ecd81bab81afe0
wonhoc <lion946@naver.com>
2025-05-13 18:13:39 +0900

    123

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,11 +7,11 @@ const migrationsPath = join(__dirname, "..", "migrations", "*.{ts,js}");
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "wonho",
-    database: "test",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "3306", 10),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: false, // 개발 환경에서만 사용 (프로덕션에서는 false로 설정)
     logging: true,
     entities: [entitiesPath],
