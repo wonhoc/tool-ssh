a2ddd8e0f8c4b4011dde89f73d191c623d9d61a8
wonhoc <lion946@naver.com>
2025-05-13 17:52:24 +0900

    123

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+
 import app from "./app";
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+    console.log(`🚀 Server running on http://localhost:${port}`);
 });
